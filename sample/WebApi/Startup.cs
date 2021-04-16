a77e1cc9f3ae737db406208dfc19ac237639b420
gurkangur <gurkangur154@gmail.com>
2021-04-16 13:18:39 +0300

    imp monitoring

--- sample/WebApi/Startup.cs
+++ sample/WebApi/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Monitoring;
 
 namespace WebApi
 {
@@ -27,7 +21,11 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            var appMetrics = Configuration.GetSection("AppMetrics").Get<AppMetricsOptions>();
+            services.AddMonitoringServices(appMetrics);
+            services.AddControllers()
+               .AddMonitoringServices();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
